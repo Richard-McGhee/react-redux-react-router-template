3e2305408a9629ea4caeb7d0aad4927dfe6f6a05
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-15 03:24:10 -0400

    Built out the template form and handlers; the submit handler needs more logic which will be added once the context examples are built up. Routing overall has been adjusted. The context child will be setup to receive state from the context hosted on the context component and will map over its array and display its default item as well as additions created by the form.

--- src/components/TempComponent.js
+++ src/components/TempComponent.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
+import { Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { placeholderAction1 } from '../actions/tempActions1'
 import { placeholderAction2 } from '../actions/tempAction2'
+import App from '../App'
 
 const TempComponent = (props) => {
 
@@ -12,6 +14,9 @@ const TempComponent = (props) => {
     
     return ( 
         <div>
+            <Link to="/">Home</Link>
+            <Route exact path="/" component={App} />
+
             {props.tempArr1.length > 0 ? <h1>{props.tempArr1[0].tempText}</h1> : <h1>Problems1</h1>}
             {props.tempArr2.length > 0 ? <h1>{props.tempArr2[0].tempText}</h1> : <h1>Problems2</h1>}
         </div>
