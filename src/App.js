75c507f79824e912ac530585d03803b5a6dfa525
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-16 23:59:33 -0400

    Got something setup for dynamic routing. It does, indeed, dynamically route. However, I have some issues that can be addressed: it does not retain new entires into its form, I would like it to only show the routed to component as well. I think once I have those issues fixed I'll setup the routed to component with an onClick handler to push the user back to the example page; I want this functionality purely for rference purpose.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TempComponent from './components/TempComponent'
 import ContextComponent from './components/ContextComponent'
+import DynamicRoutingExample from './components/DynamicRoutingExample'
 import { Route, Link } from 'react-router-dom'
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
     <div>
       <Link to="/formExample">Form Example</Link>
       <Link to="/componentExample">Component Example</Link>
+      <Link to="/dynamicRoutingExample">Dynamic Routing Example</Link>
       <Route path="/formExample" component={ContextComponent} />
       <Route path="/componentExample" component={TempComponent} />
+      <Route path="/dynamicRoutingExample" component={DynamicRoutingExample} />
     </div>
   );
 }
