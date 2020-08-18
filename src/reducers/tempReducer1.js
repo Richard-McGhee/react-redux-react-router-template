2d97ecd293334308abc1ace06a76ec64a3d7622b
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-18 01:20:31 -0400

    Implemented thunk usage in tempActions 1 and displayed its API data inside of the ReduxComponent. However, I need to update the usage of said data to give a better example of how it can be used.

--- src/reducers/tempReducer1.js
+++ src/reducers/tempReducer1.js
@@ -1,4 +1,9 @@
-import { PLACEHOLDER_ACTION1 } from '../actions/tempActions1'
+import { 
+    PLACEHOLDER_ACTION1, 
+    GET_USER_START, 
+    GET_USER_SUCCESS, 
+    GET_USER_ERROR 
+} from '../actions/tempActions1'
 
 const initState = {
     error: "",
@@ -8,6 +13,26 @@ const initState = {
 
 export function tempReducer1(state = initState, action){
     switch(action.type){
+
+        case GET_USER_START:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                tempArr: [...state.tempArr, action.payload]
+            }
+
+        case GET_USER_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
 
         case PLACEHOLDER_ACTION1:
             return {
